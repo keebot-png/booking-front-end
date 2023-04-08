35df6ce2a57d9f7f7ce84b4e359ade697d759a9c
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-04-08 22:10:42 +0000

    Add dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import LoginForm from "./components/LoginForm";
+import './stylesheets/index.css'
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <LoginForm />
+
   );
 }
