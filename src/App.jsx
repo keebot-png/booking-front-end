263a26ff5c953e76ed91f1daa99c33fbc73bda90
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-04-10 08:32:55 +0000

    Configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import LoginForm from "./components/LoginForm";
+import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-
-// import HomePage from './components/Homepage';
-// import MyBookings from './components/MyBookings';
-// import Teachers from './components/Teachers';
-// import BookingForm from './components/BookingForm';
-// import Sidebar from './components/Sidebar';
+import DashboardLayout from './components/layouts/DashboardLayout';
+import HomePage from './components/Homepage';
+import MyBookings from './components/MyBookings';
+import Teachers from './components/Teachers';
+import BookingForm from './components/BookingForm';
+import Sidebar from './components/Sidebar';
 import './stylesheets/sidebar.css';
 import './stylesheets/index.css'
 import './stylesheets/App.css';
@@ -25,11 +25,20 @@ function App() {
             <Route path="/teachers" element={<Teachers />} />
           </Routes>
         </div> */}
-         <LoginForm />
-         <Routes>
-         <Route path="/register" element={<RegisterForm />} />
-         </Routes>
 
+         
+         <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route element={<DashboardLayout />}>
+              <Route path="/dashboard" element={<HomePage />} />
+              <Route path="/my-bookings" element={<MyBookings />} />
+              <Route path="/booking-form" element={<BookingForm />} />
+              <Route path="/teachers" element={<Teachers />} />
+
+            </Route>
+
+         </Routes>
 
       </BrowserRouter>
     </>
