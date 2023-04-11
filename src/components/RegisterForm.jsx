4f99927d6ae3a649c273d15a1a02b2121f752b71
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-04-11 17:49:51 +0000

    Handle Register state with redux

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,44 +1,32 @@
-import { useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import cn from "classnames";
+import { useRef } from "react";
+import { Link, useLocation, Navigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUserAsync } from "../features/auth/authSlice";
+import useAuth from "../hooks/useAuth";
 
 const RegisterForm = () => {
-  const navigate = useNavigate();
-
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const token = useAuth()
+  const isRegistering = useSelector((state) => state.auth.isLoading);
   const {
     register,
     watch,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: 'onBlur' });
+  } = useForm({ mode: "onBlur" });
   const password = useRef({});
-  password.current = watch('password', '');
+  password.current = watch("password", "");
+
   const onSubmit = (data) => {
-    axios
-      .post(
-        'http://localhost:3000/users',
-        {
-          user: {
-            name: data.username,
-            email: data.email,
-            password: data.password,
-          },
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      )
-      .then((response) => {
-        window.localStorage.setItem(
-          'authToken',
-          response.headers.authorization,
-        );
-        navigate('/dashboard');
-      });
+    dispatch(registerUserAsync(data));
   };
+
+  if (token) {
+    return <Navigate to="/dashboard" replace state={{ from: location }} />;
+  }
 
   return (
     <div className="w-full px-5 py-24 text-gray-600 justify-self-center bg-lime-400 sm:justify-self-end sm:py-16 min-h-screen">
@@ -51,7 +39,7 @@ const RegisterForm = () => {
             <input
               type="text"
               name="username"
-              {...register('username', { required: true })}
+              {...register("username", { required: true })}
               className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-lime-400 focus:border-transparent"
               placeholder="Username"
             />
@@ -60,17 +48,16 @@ const RegisterForm = () => {
             <input
               type="text"
               name="email"
-              {...register('email', { required: true })}
+              {...register("email", { required: true })}
               className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-lime-400 focus:border-transparent"
               placeholder="Email"
             />
           </div>
-
           <div className="mb-2">
             <input
               type="password"
               name="password"
-              {...register('password', { required: true })}
+              {...register("password", { required: true })}
               className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-lime-400 focus:border-transparent"
               placeholder="Password"
             />
@@ -80,8 +67,9 @@ const RegisterForm = () => {
             <input
               type="password"
               name="confirmation"
-              {...register('password_confirmation', {
-                validate: (value) => value === password.current || 'The passwords do not match',
+              {...register("password_confirmation", {
+                validate: (value) =>
+                  value === password.current || "The passwords do not match",
               })}
               className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-lime-400 focus:border-transparent"
               placeholder="Confirm Password"
@@ -93,9 +81,13 @@ const RegisterForm = () => {
           <div className="flex items-center justify-between mt-4">
             <button
               type="submit"
-              className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in rounded-lg shadow-md disabled:opacity-50 bg-lime-800 focus:ring-lime-400 focus:ring-offset-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 enabled:disabled:hover:bg-lime-500"
+              className={cn(
+                "w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in rounded-lg shadow-md bg-lime-800 focus:ring-lime-400 focus:ring-offset-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 enabled:disabled:hover:bg-lime-500",
+                { "disabled:opacity-50": isRegistering }
+              )}
+              disabled={isRegistering}
             >
-              Signup
+              {isRegistering ? "Creating account..." : "Signup"}
             </button>
           </div>
         </form>
