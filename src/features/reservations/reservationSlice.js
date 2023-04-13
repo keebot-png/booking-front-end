b008354d23f5f6003462fba39e810d5ed576a0a0
keebot-png <kee.bot@icloud.com>
2023-04-13 11:36:03 +0200

    Add reservations and modal.

---
+++ src/features/reservations/reservationSlice.js
@@ -0,0 +1,80 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosInstance from "../../axiosConfig";
+
+const initialState = {
+    reservations: [],
+    isLoading: false,
+    error: ''
+}
+
+export const fetchReservations = createAsyncThunk(
+    'reservations/fetchReservations',
+    async({ course_id, teacher_id }) => {
+        const response = await axiosInstance.get(`/api/v1/courses/${course_id}/teachers/${teacher_id}/reservations`);
+        return response.data;
+    }
+);
+
+export const addReservations = createAsyncThunk(
+    'reservations/addReservations',
+    async(reservation) =>{
+        const { course_id, teacher_id } = reservation;
+        const response = await axiosInstance.post(`/api/v1/courses/${course_id}/teachers/${teacher_id}/reservations`, reservation);
+        return response.data;
+    }
+);
+
+export const deleteReservations = createAsyncThunk(
+    'reservations/deleteReservations',
+    async(reservation) =>{
+        const { course_id, teacher_id, id } = reservation;
+        const response = await axiosInstance.delete(`/api/v1/courses/${course_id}/teachers/${teacher_id}/reservations/${id}`);
+        return { id: id };
+    }
+);
+
+const reservationSlice = createSlice({
+    name: 'reservations',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchReservations.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(fetchReservations.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.reservations = action.payload;
+          })
+          .addCase(fetchReservations.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message;
+          })
+          .addCase(addReservations.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(addReservations.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.reservations = [...state.reservations, action.payload];
+          })
+          .addCase(addReservations.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message;
+          })
+          .addCase(deleteReservations.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(deleteReservations.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.reservations = state.reservations.filter(
+              (reservation) => reservation.id !== action.payload
+            );
+          })
+          .addCase(deleteReservations.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message;
+          });
+    }
+})
+
+export default reservationSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
+import reservationReducer from './features/reservations/reservationSlice'
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    reservations: reservationReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
