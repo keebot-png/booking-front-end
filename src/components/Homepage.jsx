3475b1eb0235d29309fdd68c3d4d7ab23901cfba
Linus Shaddai <awajisomegak@gmail.com>
2023-04-14 06:34:05 +0100

    Refactor css

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import getCourses from '../features/courses/courseApi';
-
-// const Homepage = () => {
-
-//   const courses = useSelector(state => state.courses.courses)
-//   console.log(courses)
-
-//   return (
-//     <div>
-//       <h1>Courses</h1>
-//     </div>
-//   )
-// }
-
-// export default Homepage;
-
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getCourses } from '../features/courses/courseApi';
@@ -32,6 +14,7 @@ function Homepage() {
 
   return (
     <div>
+      <h1>Welcome to Educateme</h1>
       {courses.length ? <CourseList courses={courses} />
         : <p>{status}</p>}
     </div>
